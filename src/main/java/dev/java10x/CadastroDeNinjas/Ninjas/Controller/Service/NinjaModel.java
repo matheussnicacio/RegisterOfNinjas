6653a488ef193e68270517271bf631822bd91db2
Matheus Nicacio <110930694+matheussnicacio@users.noreply.github.com>
2025-04-16 16:16:24 -0300

    Lombok

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
@@ -2,6 +2,9 @@ package dev.java10x.CadastroDeNinjas.Ninjas.Controller.Service;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -10,6 +13,12 @@ import java.util.List;
 @Entity
 @Table(name = "tb_cadastro")
 
+// Cria construtor noArgs
+@NoArgsConstructor
+// Cria construtor allArgs
+@AllArgsConstructor
+// Cria todos os GETTER e SETTERS
+@Data
 public class NinjaModel {
 
     @Id
@@ -28,36 +37,5 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") // Foreing Key ou chave estrangeira
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
