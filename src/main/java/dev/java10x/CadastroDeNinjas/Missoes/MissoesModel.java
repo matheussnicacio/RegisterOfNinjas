786f2f3ada5db7a02d3bd548add0031ffcd3a722
Matheus Nicacio <110930694+matheussnicacio@users.noreply.github.com>
2025-04-23 13:03:15 -0300

    fix: arrumando bug JPA

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,22 +1,21 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
-import dev.java10x.CadastroDeNinjas.Ninjas.Controller.Service.NinjaModel;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
 @Entity
-@Table(name = "tb_missoes")
-
-// Cria construtor noArgs
-@NoArgsConstructor
-// Cria construtor allArgs
-@AllArgsConstructor
-// Cria todos os GETTER e SETTERS
+@Table (name = "tb_missoes")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class MissoesModel {
 
     @Id
@@ -27,14 +26,9 @@ public class MissoesModel {
 
     private String dificuldade;
 
-    private NinjaModel ninja;
-
-    // @OneToMany - Uma missao pode ter varios ninjas
+    //@OneToMany - Uma missao pode ter varios ninjas
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninjas;
-
-    public MissoesModel() {
-
-    }
 
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,4 +1,4 @@
-package dev.java10x.CadastroDeNinjas.Ninjas.Controller.Service;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NinjaRepository extends JpaRepository<NinjaModel,Long> {
